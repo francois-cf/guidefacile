1df911077f32296170ef566dbe4ea2e33fa188a7
francois-cf <flrqpro@gmail.com>
2025-09-04 21:35:05 +0200

    Update generate.py

--- scripts/generate.py
+++ scripts/generate.py
@@ -80,7 +80,9 @@ def build_page(p: dict) -> None:
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="{html.escape(meta_desc)}">
   <link rel="stylesheet" href="../assets/style.css">
-  <link rel="icon" href="../favicon.png" type="image/png">
+  <link rel="icon" href="../favicon.ico" type="image/x-icon">
+<link rel="icon" type="image/png" sizes="32x32" href="../favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../favicon-16x16.png">
 
 </head>
 <body>
